354d1e945d5d757842f9976567a51efa81482eee
Joe Heitkamp <noetic97@gmail.com>
2017-05-10 17:41:02 -0600

    finish frog tests

--- test/Frog-test.js
+++ test/Frog-test.js
@@ -12,11 +12,14 @@ describe(' testing Frog', () => {
     x: 100,
     y: 100,
     width: 100,
-    height: 100
+    height: 100,
+    vX: 5
   }
 
   const obstacleArray = [ obstacle ]
   console.log(obstacleArray);
+
+  // const alert = window.alert()
 
   it('Should create a new frog', () => {
     expect(frogger).to.be.an.instanceof(Frog)
@@ -35,10 +38,12 @@ describe(' testing Frog', () => {
     frogger.move({keyCode: 37}, canvas)
     expect(frogger.x).to.equal(150);
   })
+
   it('should move down on down arrow keypress', () => {
     frogger.move({keyCode: 40}, canvas)
     expect(frogger.y).to.equal(200);
   })
+
   it('should move up on up arrow keypress', () => {
     frogger.move({keyCode: 38}, canvas)
     expect(frogger.y).to.equal(150);
@@ -50,11 +55,13 @@ describe(' testing Frog', () => {
     frogger.move({keyCode: 38}, canvas)
     expect(frogger.y).to.equal(99);
   })
+
   it('should move frog to game start position', () => {
     frogger.resetFrog();
     expect(frogger.x).to.equal(350)
     expect(frogger.y).to.equal(650)
   })
+
   it('should add a life when the score reaches multiples of 300 and frog.y is less than 50', () => {
     let gameboard = { score: 300 };
     const frogger = new Frog(0, 50)
@@ -65,6 +72,7 @@ describe(' testing Frog', () => {
     frogger.extraLife(gameboard)
     expect(frogger.startLifes).to.equal(5);
   })
+
   it('should return true when game piece overlaps with object', () => {
     const frogger = new Frog(100, 100);
     const frogger1 = new Frog(650, 650);
@@ -72,12 +80,45 @@ describe(' testing Frog', () => {
     expect(frogger.collisionHelper(obstacleArray)).to.be.true
     expect(frogger1.collisionHelper(obstacleArray)).to.not.be.true
   })
+
   it('should lose a life when hit by a car', () => {
     const frogger = new Frog(100, 100);
 
     expect(frogger.startLifes).to.equal(3);
     frogger.frogHitByCar(obstacleArray);
     expect(frogger.startLifes).to.equal(2);
+  })
 
+  it('frog should change its velocity to match the log it lands on', () => {
+    const frogger = new Frog(100, 100);
+
+    expect(frogger.vX).to.equal(0);
+    frogger.logCollisionDetection(obstacleArray);
+    expect(frogger.vX).to.equal(5);
+  })
+
+  it('frog should lose a life if it lands in the water', () => {
+    const frogger = new Frog(250, 250);
+
+    expect(frogger.startLifes).to.equal(3);
+    frogger.logCollisionDetection(obstacleArray);
+    expect(frogger.startLifes).to.equal(2);
+  })
+
+  it('game should end when frog has 0 livesand then reset the lives to 3', () => {
+    const frogger = new Frog(100, 100);
+
+    expect(frogger.startLifes).to.equal(3);
+    frogger.startLifes--;
+    frogger.gameOverCheck();
+    expect(frogger.startLifes).to.equal(2);
+    frogger.startLifes--;
+    frogger.gameOverCheck();
+    expect(frogger.startLifes).to.equal(1);
+    frogger.startLifes--;
+    expect(frogger.startLifes).to.equal(0);
+    expect(frogger).to.respondTo('gameOverCheck')
+    frogger.gameOverCheck();
+    expect(frogger.startLifes).to.equal(3);
   })
 })
