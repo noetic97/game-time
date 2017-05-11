b7db7bc05690485743d9fb4b5dcb16ebc9ba37c8
James Sullivan <jamesfsullivan5@gmail.com>
2017-05-10 21:48:22 -0600

    Add final car tests and fix typo in move cars.resetPosition()

--- test/Car-test.js
+++ test/Car-test.js
@@ -5,20 +5,31 @@ describe(' testing Car', () => {
   const cars = new Cars(10, 5, 100, 100, -4)
 
   it('should instantiate a new Car', () => {
-    expect(cars).to.be.an.instanceof(Cars)
+    expect(cars).to.be.an.instanceof(Cars);
   })
 
   it('should reset the x position of the car when x is below or equal to -50 or above or equal to 850', () => {
-    if (this.x <= -50) {
-      expect(cars.resetPostition()).to.equal(this.x = 850)
-    } else if (this.x >= 850) {
-      expect(cars.resetPostition()).to.equal(this.x = -50)
-    }
+    let car1 = new Cars(-50, 100);
+    let car2 = new Cars(850, 100);
+
+    expect(car1.x).to.equal(-50);
+    expect(car2.x).to.equal(850);
+    car1.resetPosition()
+    car2.resetPosition()
+    expect(car1.x).to.equal(850);
+    expect(car2.x).to.equal(-50);
   })
 
-  it('should increase the velocity by adding a positive or negative value to the x position', () => {
-    expect(cars.moveCars()).to.increase(cars.x, -4)
+  it('the cars should move', () => {
+    expect(cars.x).to.equal(10);
+    cars.moveCars();
+    expect(cars.x).to.equal(6);
   })
 
+  it('should create an array of 12 cars', () => {
+    let carsArray = cars.generateCarsArray();
+
+    expect(carsArray.length).to.equal(12);
+  })
 
 })
